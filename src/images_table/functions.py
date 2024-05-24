cb566dbe1dc204c098524008c83b3154adb89ac1
Almaz <79905215+almazgimaev@users.noreply.github.com>
2024-05-24 17:46:47 +0200

    Upgrade SDK to 6.73.90. Improvements and bug fixes (#20)

--- src/images_table/functions.py
+++ src/images_table/functions.py
@@ -1,49 +1,50 @@
-
 import src.images_table.widgets as card_widgets
 import src.sly_globals as g
 from supervisely.app import DataJson, StateJson
 
-import supervisely
+import supervisely as sly
 
 
 def fill_table(images_list):
-    columns = [
-        'id',
-        'dataset name',
-        'item name',
-        'image',
-        'objects number',
-        'show'
-    ]
+    columns = ["id", "dataset name", "item name", "image", "objects number", "show"]
     content = []
     for image_info in images_list:
-        ann_tool_link = f'/app/images/{g.TEAM_ID}/{g.WORKSPACE_ID}/{g.project["project_id"]}/{image_info.dataset_id}?page=1#image-{image_info.id}'
-        content.append([
-            image_info.id,
-            g.ds_id_to_name[image_info.dataset_id],
-            image_info.name,
-            f'<a href="{ann_tool_link}" rel="noopener noreferrer" target="_blank">open in annotaion tool<i class="zmdi zmdi-open-in-new" style="margin-left: 5px"></i></a>',
-            image_info.labels_count,
-            f'<a href="javascript:;">PREVIEW</a>'
-        ])
+        server_address = g.api.server_address.rstrip("/")
+        if sly.is_development():
+            url = f"{server_address}/app/images2/?datasetId={image_info.dataset_id}&imageId={image_info.id}"
+        else:
+            url = f"/app/images2/?datasetId={image_info.dataset_id}&imageId={image_info.id}"
+        text = "open in annotaion tool"
+        icon = "<i class='zmdi zmdi-open-in-new' style='margin-left: 5px'></i>"
+        link = f'<a href="{url}" rel="noopener noreferrer" target="_blank">{text}{icon}</a>'
+        content.append(
+            [
+                image_info.id,
+                g.ds_id_to_name[image_info.dataset_id],
+                image_info.name,
+                link,
+                image_info.labels_count,
+                f'<a href="javascript:;">PREVIEW</a>',
+            ]
+        )
     if len(content) > 0:
-        card_widgets.images_table.read_json({'data': content, 'columns': columns})
+        card_widgets.images_table.read_json({"data": content, "columns": columns})
     else:
-        card_widgets.images_table.read_json({'data':[], 'columns':[]})
+        card_widgets.images_table.read_json({"data": [], "columns": []})
 
 
 def stringify_label_tags(tags):
-    final_message = ''
+    final_message = ""
 
     for tag in tags:
-        value = ''
+        value = ""
         if tag.value is not None:
-            if tag.meta.value_type == str(supervisely.TagValueType.ANY_NUMBER):
+            if tag.meta.value_type == str(sly.TagValueType.ANY_NUMBER):
                 value = f":{round(tag.value, 3)}"
             else:
                 value = f":{tag.value}"
 
-        final_message += f'{tag.name}{value}<br>'
+        final_message += f"{tag.name}{value}<br>"
 
     return final_message
 
@@ -54,14 +55,17 @@ def show_preview(image_id):
 
     image_info = g.api.image.get_info_by_id(image_id, force_metadata_for_links=False)
     ann_json = g.api.annotation.download_json(image_id)
-    StateJson()['current_item_name'] = image_info.name
-    ann = supervisely.Annotation.from_json(ann_json, g.project["project_meta"])
-    img_url = image_info.full_storage_url
+    StateJson()["current_item_name"] = image_info.name
+    ann = sly.Annotation.from_json(ann_json, g.project["project_meta"])
+    server_address = g.api.server_address.rstrip("/")
+    image_path = image_info.path_original
+    if sly.is_development():
+        img_url = f"{server_address}/{image_path.lstrip('/')}"
+    else:
+        img_url = image_path
 
     card_widgets.images_gallery.append(
-        image_url=img_url,
-        title=stringify_label_tags(ann.img_tags),
-        annotation=ann
+        image_url=img_url, title=stringify_label_tags(ann.img_tags), annotation=ann
     )
 
     card_widgets.images_gallery.loading = False
